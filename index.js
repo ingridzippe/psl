c896189f06cc68ec1f4cf03a6434999da6d0bb6a
Ingrid Zippe <ingrid_zippe@brown.edu>
2019-07-30 23:59:27 -0700

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const axios = require('axios');
 
 const bizSdk = require('facebook-nodejs-business-sdk');
 const accessToken = 'your-access-token';
@@ -11,18 +12,58 @@ const AdAccount = bizSdk.AdAccount;
 const Campaign = bizSdk.Campaign;
 
 const account = new AdAccount(accountId);
+
+// const dummyDb = require('./db/db');
+
 var campaigns;
 
 var AdWords = require('googleads-node-lib');
 
-account.read([AdAccount.Fields.name])
-  .then((account) =>{
-    return account.getCampaigns([Campaign.Fields.name], { limit: 10 }) // fields array and params
-  })
-  .then((result) =>{
-    campaigns = result
-    campaigns.forEach((campaign) =>console.log(campaign.name))  
-  }).catch(console.error);
+console.log("DATABASE");
+// console.log(dummyDb);
+
+
+// account.read([AdAccount.Fields.name])
+//   .then((account) =>{
+//     return account.getCampaigns([Campaign.Fields.name], { limit: 10 }) // fields array and params
+//   })
+//   .then((result) =>{
+//     campaigns = result
+//     campaigns.forEach((campaign) =>console.log(campaign.name))  
+//   }).catch(console.error);
+
+// axios.get('/', {
+//   cancelToken: source.token
+// }).catch(function (thrown) {
+//   if (axios.isCancel(thrown)) {
+//     console.log('Request canceled', thrown.message);
+//   } else {
+//     // handle error
+//   }
+// });
+
+// axios.post('https://safe-shore-59113.herokuapp.com/instapage', {
+// 	firstName: 'Fred',
+// 	lastName: 'Flintstone'
+//   })
+//   .then(function (response) {
+// 	console.log(response);
+//   })
+//   .catch(function (error) {
+// 	console.log(error);
+//   });
+
+// app.post('/instapage', (req, res) => {
+// 	res.status(200).send({
+// 		success: 'true',
+// 		message: 'todos retrieved successfully',
+// 		todos: db
+// 	})
+// });
+
+// axios.get('https://safe-shore-59113.herokuapp.com/')
+
+// axios.post('https://safe-shore-59113.herokuapp.com/instapage')
  
 // var Service = new AdWords.ManagedCustomerService({
 //   ADWORDS_CLIENT_ID: 'your client id',
