24383fd1b31f00fe931c03d42fcafbac70ab1449
Ingrid Zippe <ingrid_zippe@brown.edu>
2019-07-31 00:20:03 -0700

    env file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const PORT = process.env.PORT || 3000
 const axios = require('axios');
 
 const bizSdk = require('facebook-nodejs-business-sdk');
@@ -90,4 +90,5 @@ app.get('/instapage', (req, res) => {
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`));
+
