b1e25e50bca98ddd10442c77a759a23e3a1b1643
Ingrid Zippe <ingrid_zippe@brown.edu>
2019-08-09 17:52:10 -0700

    ga

--- index.js
+++ index.js
@@ -67,6 +67,11 @@ axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 app.get('/instapage', (req, res) => {
   console.log("REGISTERING GET");
   console.log("button clicked get / instapage");
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: 'click',
+    eventLabel: event.target.href
+  });
 	res.status(200).send({
 		success: 'true',
 		message: 'todos retrieved successfully'
@@ -76,6 +81,11 @@ app.get('/instapage', (req, res) => {
 app.post('/instapage', (req, res) => {
   console.log("REGISTERING POST");
   console.log("button clicked post / instapage");
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: 'click',
+    eventLabel: event.target.href
+  });
   res.status(200).send({
     success: 'true',
     message: 'todos retrieved successfully'
